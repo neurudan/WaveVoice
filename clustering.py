fc592808d7e58808da2b6ffa9d6a08a2fa5c6dfc
Daniel Neururer <danielxiv64@gmail.com>
2019-11-22 14:01:02 +0100

    eer should work now

--- clustering.py
+++ clustering.py
@@ -1,5 +1,3 @@
-from scipy.cluster.hierarchy import fcluster, linkage
-from scipy.spatial.distance import cdist
 from scipy.optimize import brentq
 from scipy.interpolate import interp1d
 
@@ -30,7 +28,7 @@ class ClusterCallback(Callback):
                 while True:
                     audio_name, samples = gen.__next__()
                     embedding = np.asarray(model.predict(np.array(samples)))
-                    embeddings[audio_name] = np.mean(embedding, axis=0)
+                    embeddings[audio_name] = np.linalg.norm(np.mean(embedding, axis=0))
             except:
                 pass
             
@@ -38,25 +36,13 @@ class ClusterCallback(Callback):
             true_scores = []
             for (label, file1, file2) in self.data_handler.test_data:
                 true_scores.append(int(label))
-                print(file1)
-                print(file2)
-                scores.append(cluster_embeddings(np.array([embeddings[file1], embeddings[file2]])))
+                scores.append(np.sum(embeddings[file1]*embeddings[file2]))
 
             fpr, tpr, thresholds = roc_curve(true_scores, scores, pos_label=1)
             eer = brentq(lambda x : 1. - x - interp1d(fpr, tpr)(x), 0., 1.)
             thresh = interp1d(fpr, thresholds)(eer)
-            wandb.log({'EER': eer, 'thresh': thresh}, step=epoch)
+            wandb.log({'EER': eer, 
+                       'thresh': thresh}, 
+                       step=epoch)
+            print(eer)
             print(time.time() - start)
-            sys.exit()
-
-
-def cluster_embeddings(embeddings, metric='cosine', method='complete'):
-    print(embeddings)
-    embeddings_distance = cdist(embeddings, embeddings, 'cosine')
-    embeddings_linkage = linkage(embeddings_distance, 'complete', 'cosine')
-
-    thresholds = embeddings_linkage[:, 2]
-    predicted_cluster = fcluster(embeddings_linkage, thresholds[0], 'distance')
-    if predicted_cluster[0] == predicted_cluster[1]:
-        return 1
-    return 0

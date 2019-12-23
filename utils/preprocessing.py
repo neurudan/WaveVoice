2060a17ae9453a5de5d1eea2f30c82c4e916822c
Daniel Neururer <danielxiv64@gmail.com>
2019-12-23 23:57:47 +0100

    bugfix

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -33,7 +33,6 @@ def vgg_spectrogram(x, fs=16000):
     mag_T = mag.T
     freq, time = mag_T.shape
     spec_mag = mag_T
-    print(spec_mag.shape)
     return spec_mag.flatten(), spec_mag.shape[1]
 
 functions = [[vgg_spectrogram, 'vgg.h5', h5py.special_dtype(vlen=np.dtype('float32'))]]
